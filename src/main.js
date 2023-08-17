4f5404014f414708c25293beb2bbe54ac6441fa4
Martin Alberti <martinalberti123@gmail.com>
2023-08-17 09:32:57 -0300

    hbs

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import routerProd from "./routes/products.routes.js";
 import routerCart from "./routes/carts.routes.js";
 
 
-const PORT = 4000;
+const PORT = 8080;
 const app = express();
 
 //server
@@ -18,11 +18,11 @@ const server = app.listen(PORT, () => {
 const io = new Server(server)
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
 app.engine("handlebars", engine())
 app.set("view engine", "handlebars" )
-app.set("views", path.resolve((__dirname), "./views"))
-console.log((__dirname),"/views")
-app.use(express.urlencoded({ extended: true }));
+app.set("views", path.resolve(__dirname, "./views"))
 
 //Coneccion de Socket.io
 io.on("connection", (socket) => {
@@ -37,10 +37,30 @@ app.use('/static', express.static(path.join(__dirname, '/public'))) //path.join(
 app.use('/api/products', routerProd)
 app.use('/api/carts', routerCart)
 
+app.get('/static', (req, res) => {
+  const user = {
+      nombre: "Lucia",
+      cargo: "Tutor"
+  }
+
+  const cursos = [
+      { numCurso: "123", dia: "LyM", horario: "Noche" },
+      { numCurso: "456", dia: "MyJ", horario: "Tarde" },
+      { numCurso: "789", dia: "S", horario: "Mañana" }
+  ]
+
+  //Indicar que plantilla voy a utilizar
+  res.render("users", {
+      titulo: "Users",
+      usuario: user,
+      rutaCSS: "users.css",
+      isTutor: user.cargo == "Tutor",
+      cursos: cursos
+  })
+
+})
 
 
-app.get("*", (req, res) => {
-  res.send("Error 404");
-});
+
 
 
